8196c15a3cabdb01438e1579db3b5c16e9cd6dac
timac11 <timac11@bk.ru>
2018-11-25 13:21:56 +0300

    add blinker and commit rollback methods

--- app/db.py
+++ app/db.py
@@ -1,7 +1,8 @@
 import flask
 import psycopg2
 import psycopg2.extras
-from app import config
+import blinker
+from app import config, app
 
 
 def get_connection():
@@ -33,3 +34,21 @@ def query_all(query, **params):
         return result
 
 
+def _rollback_db(sender, exception, **extra):
+    if hasattr(flask.g, 'dbconn'):
+        conn = flask.g.dbconn
+        conn.rollback()
+        conn.close()
+        delattr(flask.g, 'dbconn')
+
+
+def _commit_db():
+    if hasattr(flask.g, 'dbconn'):
+        conn = flask.g.dbconn
+        conn.commit()
+        conn.close()
+        delattr(flask.g, 'dbconn')
+
+
+flask.got_request_exception.connect(_rollback_db, app)
+flask.request_finished.connect(_commit_db, app)
